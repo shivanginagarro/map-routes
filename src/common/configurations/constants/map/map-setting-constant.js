bc71ba67a4b522c203676ce8c08e601d38613b8a
Shivangi Upadhyay <shivangi.upadhyay@nagarro.com>
2019-03-11 20:19:07 +0530

    revised code.

--- src/common/configurations/constants/map/map-setting-constant.js
+++ src/common/configurations/constants/map/map-setting-constant.js
@@ -1,7 +1,6 @@
 export const MAP_SETTING = {
     //Put the your google console api key here...
-    // KEY:"_place_your_key_here",  
-    KEY:"AIzaSyCyQMiS-nJ0P5tvq-mL9N5qYOiY5nlNCxI",    
+    KEY:"_place_your_key_here",  
     //place the coordinates of your city... :)
     COORDINATES: {
         lat: 28.7041, 
